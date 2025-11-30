b7be84257d5c9fe157f0d92eadc6b11e8e6d98ab
reva <reva@gaiada.com>
2025-11-30 22:56:10 +0800

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,19 @@
-import gsap from 'gsap'
-import { ScrollSmoother } from 'gsap/ScrollSmoother'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 
 ScrollSmoother.create({
   content: '#smooth-content',
   wrapper: '#smooth-wrapper',
   smooth: 1.5,
   effects: true,
-  speed: .8
-})
+  speed: 0.8,
+});
 
 // Your app code
-import "swiper/swiper.css"
-import "./components/hero-image"
-import "./components/product-highlight"
+import 'swiper/swiper.css';
+import './components/hero-image';
+import './components/product-highlight';
+import './components/photo-gallery';
